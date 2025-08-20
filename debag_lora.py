fae536fa4c3bcec652993a6d9105550a8e6f067f
MurayamaTaro <muramura10taro@gmail.com>
2025-08-20 08:09:53 +0000

    .

--- debag_lora.py
+++
@@ -1,7 +0,0 @@
-import torch, re
-sd = torch.load("checkpoints/Cosmos-Predict1-7B-Text2World_post-trained-lora/model.pt", map_location="cpu")["model"]
-
-print(list(sd.keys()))
-
-w = sd["net.blocks.block0.blocks.0.block.attn.to_q.0.weight"]
-print("mean", w.mean().item(), "std", w.std().item())
